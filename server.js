bac011f9a1509348da7ecd9b65d09b0f9835900a
Marque Ray <mray2k4@yahoo.com>
2023-12-12 12:03:21 -0800

    Created internal Api

--- server.js
+++ server.js
@@ -10,6 +10,43 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'static/index.html'))
 })
 
+app.get('/api/character', function(req, res) {
+    // res.send(character)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+const character = {
+	"name": "Luke Skywalker",
+	"height": "172",
+	"mass": "77",
+	"hair_color": "blond",
+	"skin_color": "fair",
+	"eye_color": "blue",
+	"birth_year": "19BBY",
+	"gender": "male",
+	"homeworld": "https://swapi.dev/api/planets/1/",
+	"films": [
+		"https://swapi.dev/api/films/2/",
+		"https://swapi.dev/api/films/6/",
+		"https://swapi.dev/api/films/3/",
+		"https://swapi.dev/api/films/1/",
+		"https://swapi.dev/api/films/7/"
+	],
+	"species": [
+		"https://swapi.dev/api/species/1/"
+	],
+	"vehicles": [
+		"https://swapi.dev/api/vehicles/14/",
+		"https://swapi.dev/api/vehicles/30/"
+	],
+	"starships": [
+		"https://swapi.dev/api/starships/12/",
+		"https://swapi.dev/api/starships/22/"
+	],
+	"created": "2014-12-09T13:50:51.644000Z",
+	"edited": "2014-12-20T21:17:56.891000Z",
+	"url": "https://swapi.dev/api/people/1/"
+}
