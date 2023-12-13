1cc0099084ad1c4895b99178e4993368b57b4f18
Marque Ray <mray2k4@yahoo.com>
2023-12-13 07:03:14 -0800

    Got everything to work

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const axios = require('axios')
 const port = 3000
 
 app.use('/static', express.static('static'))
@@ -12,41 +13,19 @@ app.get('/', (req, res) => {
 
 app.get('/api/character', function(req, res) {
     // res.send(character)
+
+    axios.get('https://swapi.dev/api/people/1')
+  .then(function (response) {
+    // handle success
+    res.send(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-const character = {
-	"name": "Luke Skywalker",
-	"height": "172",
-	"mass": "77",
-	"hair_color": "blond",
-	"skin_color": "fair",
-	"eye_color": "blue",
-	"birth_year": "19BBY",
-	"gender": "male",
-	"homeworld": "https://swapi.dev/api/planets/1/",
-	"films": [
-		"https://swapi.dev/api/films/2/",
-		"https://swapi.dev/api/films/6/",
-		"https://swapi.dev/api/films/3/",
-		"https://swapi.dev/api/films/1/",
-		"https://swapi.dev/api/films/7/"
-	],
-	"species": [
-		"https://swapi.dev/api/species/1/"
-	],
-	"vehicles": [
-		"https://swapi.dev/api/vehicles/14/",
-		"https://swapi.dev/api/vehicles/30/"
-	],
-	"starships": [
-		"https://swapi.dev/api/starships/12/",
-		"https://swapi.dev/api/starships/22/"
-	],
-	"created": "2014-12-09T13:50:51.644000Z",
-	"edited": "2014-12-20T21:17:56.891000Z",
-	"url": "https://swapi.dev/api/people/1/"
-}
